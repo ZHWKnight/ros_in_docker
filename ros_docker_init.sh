5634121ab3d624762eebee167b3ea95336ea1a29
ZHWKnight <zhw@mail.zhw.link>
2025-02-27 10:15:30 +0800

    新增 sudoers 文件映射以解决容器内 sudo 权限问题

--- ros_docker_init.sh
+++ ros_docker_init.sh
@@ -16,6 +16,7 @@ else
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume="/etc/sudoers:/etc/sudoers:ro" \
     --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="$HOME/Worksp/ros1_noetic_ws:$HOME/Worksp/ros1_ws:rw" \
